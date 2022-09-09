0212be31ad5f431d933224c700286808c5e6ee33
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-09-09 23:05:46 +0100

    fit: equal to issue

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-(function() {
+(function () {
+  let screen = document.querySelector('.screen');
+  let buttons = document.querySelectorAll('.btn');
+  let clear = document.querySelector('.btn-clear');
+  let equal = document.querySelector('.btn-equal');
 
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+      // check if button class is equal to
+      if (e.target.classList.contains('btn-equal')) {
+        console.log('oh its equal');
+        // continue elizabeth process
+        if (screen.value === '') {
+          screen.value = 'enter value';
+        } else {
+          let answer = eval('screen.value');
+          screen.value = answer;
+          console.log(screen.value);
+        }
+      } else {
+        let value = e.target.dataset.num;
+        screen.value += value;
+      }
+    });
+  });
 
-
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function(e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-        })
-    });   
-    
-    equal.addEventListener('click', function(e) {
+  /* equal.addEventListener( 'click', function ( e ) {
         if(screen.value === '') {
             screen.value = "enter value";
         } else {
@@ -21,11 +32,10 @@
             screen.value = answer;
             console.log(screen.value);
 
-        }
-    })
-    
-    clear.addEventListener('click', function(e){
-    screen.value = '';
-    })
+        } 
+    })*/
 
+  clear.addEventListener('click', function (e) {
+    screen.value = '';
+  });
 })();
